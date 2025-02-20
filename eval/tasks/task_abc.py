d074a99bcdf4225b5b7cf238c97b729a976b7463
Mart <mart@paperplane.ai>
2025-02-20 11:27:37 +0000

    task updates

--- eval/tasks/task_abc.py
+++ eval/tasks/task_abc.py
@@ -1,12 +1,14 @@
 from typing import Any, Dict, List
 from env.src.entities import Inventory, Entity
 from env.src.instance import FactorioInstance
+from agents import TaskResponse
 
 class TaskABC:
-    def __init__(self, trajectory_length, starting_inventory: Inventory, task: str):
+    def __init__(self, trajectory_length, starting_inventory: Inventory, goal_description: str, task_key: str):
         self.trajectory_length = trajectory_length
         self.starting_inventory = starting_inventory
-        self.task = task
+        self.goal_description = goal_description
+        self.task_key = task_key
     
     def verify(self, score: float, step: int, instance: FactorioInstance, step_statistics: Dict) -> bool:
         """ Return true is the task is completed"""
@@ -14,3 +16,7 @@ class TaskABC:
     def setup(self, instance):
         """Code to provision the task environment"""
         pass
+    
+    def enchance_response_with_task_output(self, response: str, task_response: TaskResponse) -> str:
+        """Add task specific information to the environment response"""
+        return response
--- eval/tasks/task_utils.py
+++ eval/tasks/task_utils.py
@@ -1,11 +1,14 @@
-from eval.tasks.throughput_task import ThroughputTask, LAB_PLAY_POPULATED_STARTING_INVENTORY
+from eval.tasks.throughput_task import ThroughputTask
+from eval.tasks.open_play_task import OpenPlayTask
 
 def initiate_task_configs(input_task):
     if input_task["task_type"] == "populated_lab_play":
-        input_task["config"]["starting_inventory"] = LAB_PLAY_POPULATED_STARTING_INVENTORY
         return ThroughputTask(**input_task["config"])
-    task_config = ThroughputTask(**input_task["config"])
-    return task_config
+    
+    elif input_task["task_type"] == "open_ended_play":
+        return OpenPlayTask(**input_task["config"])
+    else:
+        raise ValueError("Task type not supported")
 
 def initialise_starting_state(instance, task):
     # reset the game state but with the new inventory
