ace5fd397c378911256fcd4a3dc1f320d1da3d79
Mart <mart@paperplane.ai>
2025-02-25 14:51:32 +0000

    added test for concrete

--- env/tests/entities/test_assemblers.py
+++ env/tests/entities/test_assemblers.py
@@ -16,6 +16,8 @@ def game(instance):
         'offshore-pump': 1,
         'pipe': 100,
         'storage-tank': 4,
+        "stone-brick": 20,
+        "iron-ore": 20 
     }
     instance.speed(10)
     instance.reset()
@@ -57,3 +59,31 @@ def test_assembler_2_connect_to_storage(game):
         print(f"Placed storage tank at {tank.position}")
         game.connect_entities(tank, ass_machine, Prototype.Pipe)
         game.instance.reset()
+
+
+
+def test_assembler_2_concrete(game):
+    
+    assembly_pos = Position(x=-37, y=-15.5)
+    game.move_to(assembly_pos)
+    ass_machine = game.place_entity(Prototype.AssemblingMachine2, position=assembly_pos, direction=Direction.LEFT)
+    game.set_entity_recipe(entity=ass_machine, prototype=Prototype.Concrete)
+    game.insert_item(Prototype.StoneBrick, ass_machine, 20)
+    game.insert_item(Prototype.IronOre, ass_machine, 20)
+
+    game.move_to(Position(x=-27, y=-15.5))
+    panel = game.place_entity(Prototype.SolarPanel, position=Position(x=-27, y=-15.5))
+    game.connect_entities(panel, ass_machine, Prototype.SmallElectricPole)
+
+    pump_pos = game.nearest(Resource.Water)
+    game.move_to(pump_pos)
+    pump = game.place_entity(Prototype.OffshorePump, position=pump_pos)
+    print(f"Placed pump at {pump_pos}")
+    game.connect_entities(pump, ass_machine, Prototype.Pipe)
+    
+    game.sleep(40)
+
+    assembly_machine = game.get_entity(Prototype.AssemblingMachine2, ass_machine.position)
+    output_inventory = assembly_machine.assembling_machine_output
+    concrete = output_inventory["concrete"]
+    assert concrete > 0
