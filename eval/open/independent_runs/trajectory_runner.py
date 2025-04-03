53ec7121190ec0024853858864e5bd1d33c91c1a
Mart <mart@paperplane.ai>
2025-04-03 20:54:13 +0100

    added task stop logic to trajectory runner and example to run config json

--- eval/open/independent_runs/trajectory_runner.py
+++ eval/open/independent_runs/trajectory_runner.py
@@ -166,7 +166,12 @@ class TrajectoryRunner:
                 print(f"Evaluated program {multiprocessing.current_process().name} - "
                       f"Model: {self.config.agent.model} - "
                       f"Iteration {iteration}/{self.config.agent.task.trajectory_length}")
-
+                if task_verification_response.success and self.config.exit_on_task_success:
+                    print(f"Task verification success: {task_verification_response.success}")
+                    completion_result = CompletionResult(step = iteration, 
+                                                         reason = CompletionReason.SUCCESS)
+                    await self.config.agent.end(program.conversation, completion_result)
+                    break
                 if not evaluated_program:
                     continue
 
