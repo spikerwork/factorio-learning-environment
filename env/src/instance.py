d731406641171e483910314e4184c90527207172
Kian Kyars <kiankyars@gmail.com>
2025-06-25 18:33:48 +0000

    adding the changes to test sleep to make it work as discussed in meeting

--- env/src/instance.py
+++ env/src/instance.py
@@ -216,6 +216,9 @@ class FactorioInstance:
         response = self.rcon_client.send_command(f'/sc game.speed = {speed}')
         self._speed = speed
 
+    def get_speed(self):
+        return self._speed
+
     def get_elapsed_ticks(self):
         response = self.rcon_client.send_command(f'/sc rcon.print(global.elapsed_ticks or 0)')
         if not response: return 0
@@ -645,11 +648,7 @@ class FactorioInstance:
             color_logic = 'if i==1 then char.color={r=0,g=1,b=0,a=1} elseif i==2 then char.color={r=0,g=0,b=1,a=1} end;'
         
         self.add_command(f'/sc global.agent_characters = {{}}; for _,c in pairs(game.surfaces[1].find_entities_filtered{{type="character"}}) do if c then c.destroy() end end; for i=1,{self.num_agents} do local char = game.surfaces[1].create_entity{{name="character",position={{x=0,y=(i-1)*2}},force=game.forces.player}}; {color_logic} global.agent_characters[i]=char end', raw=True)
-        self.execute_transaction()
-
-    def speed(self, speed): 
-        response = self.rcon_client.send_command(f'/sc game.speed = {speed}')
-        self._speed = speed
+        self.execute_transaction()\
 
     def get_warnings(self, seconds=10):
         """
